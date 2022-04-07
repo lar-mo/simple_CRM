ef0853d16eace4c0416174eccebb810c677883a7
Larry Moiola <lmoiola@gmail.com>
2022-04-07 08:47:38 -0700

    .

--- councilProj/urls.py
+++ councilProj/urls.py
@@ -6,6 +6,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^login/$',auth_views.LoginView.as_view(template_name="members/login.html"), name="login"),
     url(r'^logout/$', auth_views.LogoutView.as_view(template_name="members/welcome.html"), name='logout'),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin-Ca7pJ6c0/', admin.site.urls),
     path('', include('members.urls')),
 ]
